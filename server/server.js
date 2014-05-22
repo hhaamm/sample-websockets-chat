53daf62a0fa736a815cc75e5ef06df09bdc81495
hhaamm <ham1988@gmail.com>
2014-05-22 12:59:39 -0300

    Chat client / server built in Node.js with WebSockets

--- server/server.js
+++ server/server.js
@@ -4,24 +4,11 @@ var app = require('http').createServer(handler)
 
 app.listen(8000);
 
-function handler (req, res) {
-    fs.readFile(
-        __dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-
-            res.writeHead(200);
-            res.end(data);
-            return null;
-        });
-}
+function handler() {}
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.on("message", function(data) {
+        console.log("New message: "+data.message);
+        socket.emit("receive-message", data);
     });
 });
